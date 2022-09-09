d76f3836daa9fc6621fc5a8ad191429bf81a47d2
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2022-09-09 13:41:31 -0700

    My seventh commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 57; z < 67; z++)
+	{
+		putchar(z);
+	}
+	putchar("\n");
+	return (0);
+}
