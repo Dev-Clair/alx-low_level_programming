8b327bb9751f2f5d30f02f509ce030eefb3ccd66
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2022-09-09 13:32:00 -0700

    My sixth commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints single digits of base 10 starting from 0
+ * main - prints all single digit numbers of base 10 starting from 0
  * followed by a new line
  * Return: 0
  */
@@ -11,9 +11,7 @@ int main(void)
 	int z;
 
 	for (z = 0; z < 10; z++)
-	{
-		printf("%d", z);
-	}
-	printf('\n');
+	printf("%d", z);
+	printf("\n");
 	return (0);
 }
