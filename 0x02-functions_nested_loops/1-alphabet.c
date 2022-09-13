a763fc55d09029c9f82c44541f08f0400e161018
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2022-09-13 09:42:59 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - 
+ * *
+ * Return (0) - Always Sucess
+ */
+
+int main(void)
+{
+	/* Code goes here */
+	printf("Hello World\n");
+	Return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains my answers to the 0x02-functions_nested_loops task questions.
